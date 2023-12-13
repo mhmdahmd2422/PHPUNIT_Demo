8678d8c421df11fea0b5b53ccdb39adff092d652
Mohamed Ahmed <mhmdahmd2422@gmail.com>
2023-12-14 00:41:58 +0200

    Refactor String Calculator Module

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -2,30 +2,45 @@
 
 namespace App;
 
+use Exception;
+
 class StringCalculator
 {
+    const MAX_NUMBER_ALLOWED = 1000;
+    protected string $delimiter = ',|\\\\n';
     public function add(string $nums)
     {
-        $delimiter = ',|\\\\n';
-        if(empty($nums)){
-            return 0;
-        }
+        $this->disallowNegatives($nums = $this->parseString($nums));
+
+        return array_sum(
+            $this->ignoreMoreThanAllowed($nums)
+        );
+    }
+
+    protected function parseString(string $nums): array
+    {
         if(preg_match('/\/\/(.)\\\\n/', $nums, $matches)){
             //set delimiter to matched regex '(.)'
-            $delimiter = $matches[1];
+            $this->delimiter = $matches[1];
             //remove custom delimiter indicator '//delimiter\n'
             $nums = str_replace($matches[0], '', $nums);
         }
-        $nums = preg_split("/{$delimiter}/", $nums);
-        foreach ($nums as $num){
-            if($num < 0){
-                throw new \Exception('negatives not allowed');
+        return preg_split("/{$this->delimiter}/", $nums);
+    }
+
+    protected function disallowNegatives(array $nums): void
+    {
+        foreach ($nums as $num) {
+            if ($num < 0 && !empty($num)) {
+                throw new Exception('negatives not allowed');
             }
         }
-        $nums = array_filter($nums, function ($nums){
-           return $nums <= 1000;
-        });
+    }
 
-        return array_sum($nums);
+    public function ignoreMoreThanAllowed(array $nums): array
+    {
+        return array_filter($nums, fn($nums) =>
+           $nums <= self::MAX_NUMBER_ALLOWED
+        );
     }
 }
